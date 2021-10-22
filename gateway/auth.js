3f2e57ca8bd1f8bb251abd396e5cd9f4a834b246
VictorLaSalle <victor.202031214@unilasalle.edu.br>
2021-10-22 20:58:24 -0300

    fix retorno

--- gateway/auth.js
+++ gateway/auth.js
@@ -1,4 +1,4 @@
 const request = require('request')
 require('../environment')
 
-module.exports = reqt = () => request.post(`${hostname}${endpointAuth}`, (err, res, body) => console.log(body)) 
+module.exports = reqt = () => request.post(`${hostname}${endpointAuth}`, (err, res, body) => body) 
