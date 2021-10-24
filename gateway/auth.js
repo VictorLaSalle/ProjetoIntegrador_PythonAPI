3419d9db03457e257e087e83a8822eeb0db847ab
VictorLaSalle <victor.202031214@unilasalle.edu.br>
2021-10-23 23:07:57 -0300

    fix auth

--- gateway/auth.js
+++ gateway/auth.js
@@ -1,10 +1,5 @@
 const request = require('request')
 require('../environment')
 
-module.exports = reqt = () => {
-    let response
-    
-    request.post(`${hostname}${endpointAuth}`, function(err, res, body) { response = res })
-    
-    return response
-}
+module.exports = reqt = () => request.post(`${hostname}${endpointAuth}`)
+
